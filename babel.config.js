0d0a8942814f3dc73244fc64bf98d1df548dd8cf
mahesh <mahesha@bellwether.org.in>
2024-12-01 10:42:04 +0530

    commit

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,8 @@
 module.exports = function (api) {
   api.cache(true);
   return {
+    "presets": ["module:metro-react-native-babel-preset"],
+
     presets: [["babel-preset-expo", {
       jsxImportSource: "nativewind"
     }], "nativewind/babel"],
