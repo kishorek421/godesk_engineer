aebbce6bdf54feb9780dd7784f2f0f72b2b5a19c
kishorek421 <kishore@bellwether.org.in>
2024-12-16 13:37:28 +0530

    pointing to prod

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,17 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { Redirect } from 'expo-router'
+import LoadingBar from '@/components/LoadingBar';
 
 const Index = () => {
+  // return (
+  //   <Redirect href="login" />
+  // )
   return (
-    <Redirect href="login" />
-  )
+    <View>
+      <LoadingBar />
+    </View>
+  );
 }
 
 export default Index
