e9cd47e112cd45e5c74f200773fa62a33b9382c4
Megavathi K <megavathi@bellwether.org.in>
2025-08-08 17:52:58 +0530

    commit

--- app/home.tsx
+++ app/home.tsx
@@ -31,6 +31,7 @@ import { useToast } from "@/context/ToastContext";
 // import useLocation from "@/hooks/useLocation";
 import { removeItem, setItem } from "@/utils/secure_store";
 import { requestForegroundPermissionsAsync } from "expo-location";
+import { TouchableWithoutFeedback } from "react-native";
 
 const HomeScreen = () => {
   const [isLoading, setIsLoading] = useState(true);
@@ -43,7 +44,7 @@ const HomeScreen = () => {
   const [checkInOutStatusDetails, setCheckInOutStatusDetails] =
     useState<CheckInOutStatusDetailsModel>({});
   const [checkInOutStatus, setCheckInOutStatus] = useState<CheckInOutStatusDetailsModel[]>([]);
-
+    const [expanded, setExpanded] = useState(false);
   const [inProgressTicketDetails, setInProgressTicketDetails] =
     useState<TicketListItemModel>({});
   const [userDetails, setUserDetails] = useState<UserDetailsModel>({});
@@ -341,28 +342,29 @@ const HomeScreen = () => {
             >
               <View className="bg-white px-4 py-3 rounded-lg w-full">
                 <View className="flex">
-                  <View className="flex-row justify-between w-full items-center">
-                    <View>
-                      <PrimaryText className="font-bold-1 text-tertiary-950 leading-5">
-                        {inProgressTicketDetails.ticketNo ?? "-"}
+                   <View className="flex-row justify-between w-full">
+                    <View className="flex-1">
+                      <PrimaryText className="text-tertiary-950 leading-5  font-bold-1">
+                        {inProgressTicketDetails?.ticketNo ?? "-"}
                       </PrimaryText>
-                      <PrimaryText
-                        className="mt-[1px] text-[13px] text-gray-900 font-regular"
-                        translate="api"
-                      >
-                        {`${t("issueIn")}: ${inProgressTicketDetails.issueTypeDetails?.name ?? "-"}`}
-                      </PrimaryText>
+                      <TouchableWithoutFeedback onPress={() => setExpanded(!expanded)}>
+                        <PrimaryText
+                          className="mt-[1px] text-[13px] text-gray-900 font-regular"
+                          translate="api"
+                          numberOfLines={expanded ? undefined : 4}
+                          ellipsizeMode="tail"
+                        >
+                          {`${t('issueIn')}: ${Array.isArray(inProgressTicketDetails.issueTypeDetails) && inProgressTicketDetails.issueTypeDetails.length > 0
+                            ? inProgressTicketDetails.issueTypeDetails.map((item) => item?.name).filter(Boolean).join(', ')
+                            : "-"
+                            }`}
+                        </PrimaryText>
+                      </TouchableWithoutFeedback>
                     </View>
-                    <View>
-                      <TicketStatusComponent
-                        statusKey={
-                          inProgressTicketDetails.statusDetails?.key ?? ""
-                        }
-                        statusValue={
-                          inProgressTicketDetails.statusDetails?.value ?? ""
-                        }
-                      />
-                    </View>
+                    <TicketStatusComponent
+                      statusKey={inProgressTicketDetails.statusDetails?.key}
+                      statusValue={inProgressTicketDetails.statusDetails?.value}
+                    />
                   </View>
                   <View className="border-[1px] border-gray-300 mt-3 mb-3 border-dashed w-full h-[1px]" />
                   <View className="w-full">
