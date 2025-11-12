0f4fe65f31921fe84f9cfb950a0aa99fd59982de
Dharani1111 <Dharani1111@gmail.com>
2025-11-12 14:50:29 +0530

    changes

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,16 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: [["babel-preset-expo", {
-      jsxImportSource: "nativewind"
-    }], "nativewind/babel"],
-    plugins: [["module-resolver", {
-      root: ["./"],
-      alias: {
-        "@": "./"
-      }
-    }]],
+    presets: ["babel-preset-expo", "react-native-css-interop/babel"],
+    plugins: [
+      "react-native-worklets-core/plugin",
+      ["module-resolver", {
+        root: ["./"],
+        alias: {
+          "@": "./"
+        }
+      }],
+      "react-native-reanimated/plugin"
+    ],
   };
 };
