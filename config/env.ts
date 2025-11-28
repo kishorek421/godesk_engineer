14c8884d404c8a4b1fc7c783598c97e559b3bb91
Suresh-1606 <sureshj@bellwether.org.in>
2025-11-28 13:14:49 +0530

    config file updated

--- config/env.ts
+++ config/env.ts
@@ -14,10 +14,10 @@ enum ENVS {
   prod,
 }
 
-const env: ENVS = ENVS.local;
+// const env: ENVS = ENVS.local;
 // const env: ENVS = ENVS.dev;
 // const env: ENVS = ENVS.stage;
-// const env: ENVS = ENVS.prod;
+const env: ENVS = ENVS.prod;
 
 const getBaseURL = (iENV: ENVS) => {
   switch (iENV) {
